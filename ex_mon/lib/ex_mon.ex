d80983b5b4e8bc1c4125c891c59a2b786011cbec
Anderson Costa <117184997+Andersoncostaf@users.noreply.github.com>
2023-02-23 11:48:47 +0000

    inserindo comentário apos revisar o codigo.

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -16,3 +16,13 @@ defmodule ExMon do
       Game.start(computer, player)
   end
 end
+
+# ---- comentário ---- #
+#Esse código em Elixir define um módulo chamado ExMon, que contém duas funções: create_player e start_game.
+
+# A primeira função, create_player, recebe quatro argumentos: name, move_avg, move_rnd e move_heal. Essa função cria um novo jogador (um objeto Player) com base nos argumentos fornecidos e retorna esse objeto.
+
+#A função create_player chama uma função Player.build para criar o objeto Player. É provável que a definição dessa função esteja em outro módulo do programa, já que o módulo atual usa alias ExMon.Player para criar um alias para o módulo Player. O alias permite que o código use o nome curto Player em vez do nome completo ExMon.Player sempre que quiser se referir a esse módulo.
+
+#A segunda função, start_game, recebe um objeto Player como argumento. Essa função cria um novo jogador (um objeto Player) chamado "Robotinik" com movimentos definidos como :punch, :kick e :heal, e, em seguida, inicia um novo jogo (um objeto Game) com esses dois jogadores como participantes. Novamente, é provável que a definição da função Game.start esteja em outro módulo do programa, já que o módulo atual usa alias ExMon.Game para criar um alias para o módulo Game.
+
--- ex_mon/lib/game.ex
+++ ex_mon/lib/game.ex
@@ -10,3 +10,15 @@ defmodule ExMon.Game do
     Agent.get(__MODULE__, & &1)
   end
 end
+
+#O módulo "ExMon.Game" utiliza a biblioteca padrão Agent porque ela é uma escolha comum para gerenciar estados compartilhados em sistemas concorrentes em Elixir. 
+
+#Este arquivo game.ex é outro exemplo de um módulo definido em Elixir que utiliza a biblioteca padrão Agent. Este módulo contém duas funções principais: "start" e "info".
+
+#A função "start" cria uma nova instância de um agente usando a função "Agent.start_link". O agente é criado com um estado inicial representado por um mapa que contém quatro chaves: "computer", "player", "turn" e "status". O valor da chave "computer" é definido pelo argumento "computer" passado para a função "start", e o valor da chave "player" é definido pelo argumento "player". A chave "turn" é inicialmente definida como ":player", indicando que é a vez do jogador jogar. A chave "status" é definida como ":started", indicando que o jogo foi iniciado.
+
+#A função "start" retorna o pid do agente criado, que pode ser usado posteriormente para enviar mensagens para o agente.
+
+#a função "info" utiliza a função "Agent.get" para obter o estado atual do agente criado pelo módulo. A função "info" retorna o estado atual do agente para o chamador.
+
+#Resumindo, o módulo "ExMon.Game" utiliza a biblioteca Agent para criar uma instância de um agente com um estado inicial e fornece métodos para obter informações do estado atual do agente. Este módulo pode ser utilizado para implementar um jogo, onde o agente representa o estado do jogo e os jogadores podem enviar mensagens para o agente para atualizar o estado do jogo.
